d329c5414f35f393cf055d35ed049a50031b425c
NureAlieksieievaKateryna <kateryna.alieksieieva@nure.ua>
2024-12-06 18:45:12 +0200

    Add files via upload

---
+++ PractTask4/java/java/nure/labtask4/entity/ImportanceItem.java
@@ -0,0 +1,20 @@
+package java.nure.labtask4.entity;
+
+
+public class ImportanceItem{
+    private String title;
+    private Integer imageResId;
+
+    public ImportanceItem(String title, Integer imageResId){
+        this.title = title;
+        this.imageResId = imageResId;
+    }
+
+    public String GetTitle(){
+        return title;
+    }
+
+    public Integer GetImageResId(){
+        return imageResId;
+    }
+}
